ddc6bce94f8a4a32c8aa5561909c7ad8a660ca41
emattias <mattiasha@gmail.com>
2013-11-13 13:22:10 +0100

    Keep admin_menu css

--- better-defaults/css.php
+++ better-defaults/css.php
@@ -1,5 +1,13 @@
 <?php
 
 function ff_css_alter(&$css){
-  $css = array();
+
+  $css_to_keep = variable_get('ff_css_to_keep', array(
+    // admin_menu
+    'sites/all/modules/admin_menu/admin_menu.css',
+    'sites/all/modules/admin_menu/admin_menu.uid1.css',
+    'sites/all/modules/admin_menu/admin_menu_toolbar/admin_menu_toolbar.css',
+  ));
+
+  $css = array_intersect_key($css, array_flip($css_to_keep));
 }
