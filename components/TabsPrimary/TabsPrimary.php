dcee174363a0d11c85a52a67a3241d3b262c2e54
emattias <mattiasha@gmail.com>
2013-11-20 13:42:14 +0100

    Add TabsPrimary hook implementation proposal

--- components/TabsPrimary/TabsPrimary.php
+++ components/TabsPrimary/TabsPrimary.php
@@ -1,3 +1,22 @@
 <?php
 
 // tabs are defined in theme_menu_local_tasks()
+
+function TabsPrimary_menu_local_tasks(&$vars) {
+  $output = '';
+
+  if (!empty($vars['primary'])) {
+    $vars['primary']['#prefix'] = '<h2 class="element-invisible">' . t('Primary tabs') . '</h2>';
+    $vars['primary']['#prefix'] .= '<ul class="tabs primary">';
+    $vars['primary']['#suffix'] = '</ul>';
+    $output .= drupal_render($vars['primary']);
+  }
+  if (!empty($vars['secondary'])) {
+    $vars['secondary']['#prefix'] = '<h2 class="element-invisible">' . t('Secondary tabs') . '</h2>';
+    $vars['secondary']['#prefix'] .= '<ul class="tabs secondary">';
+    $vars['secondary']['#suffix'] = '</ul>';
+    $output .= drupal_render($vars['secondary']);
+  }
+
+  return $output;
+}
