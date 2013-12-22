76ed0cd1bc818828ca2336c823440d34a21efa9f
Moa Nandorf <moa.nandorf@gmail.com>
2013-12-22 17:16:47 +0100

    Compontents: TabsPrimary: Add SUIT classes.

--- components/TabsPrimary/TabsPrimary.php
+++ components/TabsPrimary/TabsPrimary.php
@@ -2,21 +2,43 @@
 
 // tabs are defined in theme_menu_local_tasks()
 
-function TabsPrimary_menu_local_tasks(&$vars) {
+// function TabsPrimary_menu_local_tasks(&$vars) {
+function ff_menu_local_tasks(&$vars) {
   $output = '';
 
   if (!empty($vars['primary'])) {
-    $vars['primary']['#prefix'] = '<h2 class="element-invisible">' . t('Primary tabs') . '</h2>';
-    $vars['primary']['#prefix'] .= '<ul class="tabs primary">';
+//    $vars['primary']['#prefix'] = '<h2 class="element-invisible">' . t('Primary tabs') . '</h2>';
+    $vars['primary']['#prefix'] = '';
+    $vars['primary']['#prefix'] .= '<ul class="Tabs Tabs--primary">';
     $vars['primary']['#suffix'] = '</ul>';
     $output .= drupal_render($vars['primary']);
   }
   if (!empty($vars['secondary'])) {
     $vars['secondary']['#prefix'] = '<h2 class="element-invisible">' . t('Secondary tabs') . '</h2>';
-    $vars['secondary']['#prefix'] .= '<ul class="tabs secondary">';
+    $vars['secondary']['#prefix'] .= '<ul class="Tabs Tabs--secondary">';
     $vars['secondary']['#suffix'] = '</ul>';
     $output .= drupal_render($vars['secondary']);
   }
 
   return $output;
+}
+
+function ff_menu_local_task($variables) {
+  $link = $variables['element']['#link'];
+  $link_text = $link['title'];
+
+  if (!empty($variables['element']['#active'])) {
+    // Add text to indicate active tab for non-visual users.
+    $active = '<span class="element-invisible">' . t('(active tab)') . '</span>';
+
+    // If the link does not contain HTML already, check_plain() it now.
+    // After we set 'html'=TRUE the link will not be sanitized by l().
+    if (empty($link['localized_options']['html'])) {
+      $link['title'] = check_plain($link['title']);
+    }
+    $link['localized_options']['html'] = TRUE;
+    $link_text = t('!local-task-title!active', array('!local-task-title' => $link['title'], '!active' => $active));
+  }
+
+  return '<li' . (!empty($variables['element']['#active']) ? ' class="Tabs-tab is-active"' : ' class="Tabs-tab"') . '>' . l($link_text, $link['href'], $link['localized_options']) . "</li>\n";
 }
